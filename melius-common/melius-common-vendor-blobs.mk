39ed69a40ed3b13a1e3811d3c8fa6eca019838a8
Silesh K Nair <sileshn@gmail.com>
2014-07-18 13:51:34 +0530

    Update NFC from Stock Kitkat

--- melius-common/melius-common-vendor-blobs.mk
+++ melius-common/melius-common-vendor-blobs.mk
@@ -137,6 +137,4 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/melius-common/proprietary/lib/libwvm.so:system/lib/libwvm.so \
     vendor/samsung/melius-common/proprietary/vendor/firmware/bcm2079xB4_firmware.ncd:system/vendor/firmware/bcm2079xB4_firmware.ncd \
     vendor/samsung/melius-common/proprietary/vendor/firmware/bcm2079xB4_pre_firmware.ncd:system/vendor/firmware/bcm2079xB4_pre_firmware.ncd \
-    vendor/samsung/melius-common/proprietary/vendor/firmware/bcm2079xB5_firmware.ncd:system/vendor/firmware/bcm2079xB5_firmware.ncd \
-    vendor/samsung/melius-common/proprietary/vendor/firmware/bcm2079xB5_pre_firmware.ncd:system/vendor/firmware/bcm2079xB5_pre_firmware.ncd
-
+	
